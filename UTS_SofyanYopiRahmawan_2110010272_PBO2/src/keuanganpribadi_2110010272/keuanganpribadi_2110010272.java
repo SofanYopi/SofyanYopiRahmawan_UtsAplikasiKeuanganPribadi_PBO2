c7de58897f0e3300139c8d96ef38470b0b9d4666
SofyanYopiRahmawan <149921224+SofanYopi@users.noreply.github.com>
2023-11-26 17:56:31 +0700

    first

---
+++ UTS_SofyanYopiRahmawan_2110010272_PBO2/src/keuanganpribadi_2110010272/keuanganpribadi_2110010272.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package keuanganpribadi_2110010272;
+import  Database.Koneksi;
+import java.sql.SQLException;
+import Forms.*;
+
+/**
+ *
+ * @author Acer
+ */
+public class keuanganpribadi_2110010272 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        new kemasan_katagori_barang().setVisible(true);
+                
+    }
+     
+}
